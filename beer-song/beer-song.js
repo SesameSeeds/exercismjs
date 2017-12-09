a8240bdb42918548997d0dca69436d53a489e20f
Stef <seedsofthesesame@gmail.com>
2017-12-09 00:59:02 -0800

    Next exercism, re-visiting my favourite childhood song.

---
+++ beer-song/beer-song.js
@@ -0,0 +1,10 @@
+const BeerSong = function verse(bottles) {
+
+};
+
+// Will need a way to calculate the bottles in to song verses.
+// Will need original verse for the song goes on FOREVER
+// Will need verses for single and zero (sad) beer verses tests 3 and 4.
+// Want a conditional that allows for the verses to go depending on bottles of beer.
+
+module.exports = BeerSong;
